56ee3227a4b8121f3f5128f30044d9978e5fda2b
Tony Zale <tonyzale@gmail.com>
2015-12-14 21:50:44 -0600

    basic channel page

--- server.js
+++ server.js
@@ -3,30 +3,43 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1337;
 
-var sqlite3 = require('sqlite3'); //.verbose();
+var urlMap = {
+    'channels' : channelMapPage
+};
+
+var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('./me-tv.db');
-
-
-const tables = ['scheduled_recording', 'channel'];
-tables.forEach(t => {
-    db.each(`SELECT * FROM ${t}`, function(err, row) {
-    console.log(row);
-    });});
 
 var channels = {};
 db.serialize(function() {
     db.each('SELECT channel_id, name FROM channel', (err, row) => {
-	channels[row.channel_id] = row.name;
+	   channels[row.name] = row.channel_id;
     });
     db.close();
 });
 
+function channelMapPage(res) {
+    res.write("<body><table>");
+    for (var name in channels) {
+        res.write(`<tr><td>${name}</td><td>${channels[name]}</td></tr>`);
+    }
+    res.write("</table></body>");
+}
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.end('Hello World\n' + JSON.stringify(channels));
+    var renderFunc = urlMap[req.url.substr(1)];
+    if (!renderFunc) {
+        res.writeHead(404, "Unknown Page");
+        res.end();
+        return;
+    } else {
+        console.log(req.url);
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        renderFunc(res);
+        res.end();
+    }
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
