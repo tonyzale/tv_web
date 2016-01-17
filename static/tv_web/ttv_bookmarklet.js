960bdce76253717d2680fd71ed9632e79560dd56
Tony Zale <tonyzale@gmail.com>
2016-01-16 20:44:15 -0600

    unexpected token '(' fix

--- static/tv_web/ttv_bookmarklet.js
+++ static/tv_web/ttv_bookmarklet.js
@@ -1,3 +1,3 @@
-function() {
+(function() {
     alert("title:" + $("#title").text() + " time:" + $("#time").text() + " channel:" + $("#channel").text() + " detail:" + $("#detail").text());
-}();
+}());
